08bbe627bbf4883ed84c0cd24e984cf1f3c581fa
Anton Kalashnikov <kalashnikov@SPSEJECNANET.CZ>
2022-12-12 09:09:41 +0100

    f

---
+++ 12122022_delegate+linq/Firm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12122022_delegate_linq
+{
+    internal class Firm
+    {
+        private string name;
+        private List<Staff> staff_list;
+
+        public Firm(string name, List<Staff> staff)
+        {
+            this.name = name;
+            this.staff_list = staff;
+        }
+
+        public Firm()
+        {
+            this.name = "Sad firm s.r.o";
+            this.staff_list = new List<Staff>
+            {
+                new Staff("Jhon","Beckham",200000,Rank.Boss,new DateTime(2000,01,23)),
+                new Staff("Don","Beckham",200000,Rank.Cap,new DateTime(2000,01,23)),
+                new Staff("Azz","Man",2000,Rank.Leutenant,new DateTime(2005,11,23)),
+                new Staff("Zan","Dam",100,Rank.Eights,new DateTime(2007,2,3))
+            };
+        }
+
+        public IEnumerable<Staff> FindMostPayed()
+        {
+            var amount = staff_list.Max(x => x.Salary);
+            var z = staff_list.OrderByDescending(x => x.Salary).TakeWhile(x => x.Salary == amount);
+
+            foreach (var staff in z)
+                yield return staff;
+        }
+
+        public double FindMidSalary()
+        {
+            return staff_list.Average(s => s.Salary);
+        }
+
+        public double FindMidSalaryForRank(Rank rank)
+        {
+            return staff_list.Where(s => s.Rank == rank).Average(s => s.Salary);
+        }
+
+        public IEnumerable<Staff> FindThreeSmallestSalary()
+        {
+            var z = staff_list.OrderBy(x => x.Salary).Take(3);
+
+            foreach (var staff in z)
+                yield return staff;
+        }
+
+    }
+
+
+    enum Rank
+    {
+        Boss,
+        Cap,
+        Leutenant,
+        Eights,
+    }
+
+    internal class Staff
+    {
+        private string name;
+        private string surname;
+        private int salary;
+        private Rank rank;
+        private DateTime started_since;
+
+        public Staff(string name, string surname, int salary, Rank rank, DateTime started_since)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.salary = salary;
+            this.rank = rank;
+            this.started_since = started_since;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public int Salary { get => salary; set => salary = value; }
+        public DateTime Started_since { get => started_since; set => started_since = value; }
+        internal Rank Rank { get => rank; set => rank = value; }
+
+        public override string ToString()
+        {
+            return Name + " : " + Surname + " : " + Salary;
+        }
+    }
+}
---
+++ 12122022_delegate+linq/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _12122022_delegate_linq
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Func<String, String> upp = str => str.ToUpper();
+            Action<int> select = x => Console.WriteLine(x);
+            Predicate<int> selec = Check;
+
+            string[] words = { "orange", "apple", "Article", "elephant" };
+            IEnumerable<String> aWords = words.Select(upp);
+            foreach (String word in aWords)
+                Console.WriteLine(word);
+
+            //List<String> list = new List<string>(words);
+            //list.ForEach();
+
+            int[] pred_test = { 1, 2, 4, 5, 6, 10 };
+            var pred_answr = Array.FindAll(pred_test, selec);
+            foreach (int i in pred_answr)
+                Console.WriteLine(i);
+
+
+
+            Firm frm = new Firm();
+            foreach (var f in frm.FindMostPayed())
+                Console.WriteLine(f.ToString());   
+        }
+
+        public static bool Check(int x)
+        {
+            return x % 5 == 0;
+        }
+    }
+}
